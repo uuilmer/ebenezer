0ff3ef0606a922eb2527a9932cf2f19df52059b7
Wilmer <wilmerh701@gmail.com>
2021-03-30 20:40:14 -0400

    Fixed: Port to listen on

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'home.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port);
