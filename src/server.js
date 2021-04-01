14ca01c81644936d4868b43fc6705f7f56e40cd6
Wilmer <wilmerh701@gmail.com>
2021-03-31 21:49:08 -0400

    Added Bootstrap Nav and background video

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require('path');
+const path = require("path");
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'home.html'));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "home.html"));
 });
 
 app.listen(port);
