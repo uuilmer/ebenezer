de22792f5711a362aed73609c6b09f52100bef45
Wilmer <wilmerh701@gmail.com>
2022-01-13 19:11:24 -0500

    Replaced: Home Page Video with Animation

--- public/components/HomePage.js
+++ public/components/HomePage.js
@@ -13,28 +13,35 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 function HomePage() {
+  /*
+    Includes code for Dynamic Home Page Title
+    Adapted from: https://www.sliderrevolution.com/resources/css-text-animation/
+    Animation: Cross Color Text Reveal
+    Developer: Kenny
+  */
   // TODO: Add dynamic quotes
-  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("video", {
-    autoPlay: true,
-    muted: true,
-    loop: true,
-    id: "desktop_home_background"
-  }, /*#__PURE__*/_react["default"].createElement("source", {
-    src: "../media/pexels_pixabay_background.mp4",
-    type: "video/mp4"
-  })), /*#__PURE__*/_react["default"].createElement("img", {
-    id: "mobile_home_background",
-    src: "../media/mobile_home_background.png",
-    type: "img/png"
-  }), /*#__PURE__*/_react["default"].createElement("div", _defineProperty({
-    className: "content"
-  }, "className", "full-parent-height"), /*#__PURE__*/_react["default"].createElement("h1", {
-    className: "display-1",
-    id: "church_name"
-  }, "\"Iglesia de Dios Ebenezer\""), /*#__PURE__*/_react["default"].createElement("div", {
+  var darkRef = (0, _react.useRef)(null);
+  var lightRef = (0, _react.useRef)(null);
+  setupDynamicTitle(darkRef, lightRef);
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    id: "home_page_container"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "outer-container"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    id: "home-image",
+    className: "image-container"
+  }, /*#__PURE__*/_react["default"].createElement("h2", {
+    className: "section-title on-dark"
+  }, /*#__PURE__*/_react["default"].createElement("span", {
+    className: "paralax-title",
+    ref: darkRef
+  }, "Iglesia de Dios Ebenezer"))), /*#__PURE__*/_react["default"].createElement("h2", {
+    className: "section-title"
+  }, /*#__PURE__*/_react["default"].createElement("span", {
+    className: "paralax-title",
+    ref: lightRef
+  }, "Iglesia de Dios Ebenezer"))), /*#__PURE__*/_react["default"].createElement("div", {
     id: "home_intro"
   }, /*#__PURE__*/_react["default"].createElement("figure", {
     className: "text-center"
@@ -44,5 +51,36 @@ function HomePage() {
     className: "display-6"
   }, "\"Tom\xF3 luego Samuel una piedra y la puso entre Mizpa y Sen, y le puso por nombre Eben-ezer\"")), /*#__PURE__*/_react["default"].createElement("figcaption", {
     className: "blockquote-footer"
-  }, "\"1 Samuel 7:12\"")))));
+  }, "\"1 Samuel 7:12\""))));
+} /// Use black and white identical title elements to add cool animation
+///
+/// @param darkRef A useRef return value reqpresenting the black title
+/// @param lightRef A useRef return value reqpresenting the white title
+
+
+function setupDynamicTitle(darkRef, lightRef) {
+  var didScroll = false;
+  /*
+    When the user scrolls, trigger the conditional statement
+    inside the infinetly repeating computeFrame function
+  */
+
+  var scrollInProgress = function scrollInProgress() {
+    didScroll = true;
+  };
+
+  var computeFrame = function computeFrame() {
+    if (didScroll) {
+      darkRef.current.style.transform = "translateX(" + window.scrollY / 10 + "%)";
+      lightRef.current.style.transform = "translateX(" + window.scrollY / 10 + "%)";
+      didScroll = false;
+    } // Recursively call computeFrame to cause constant frame computation
+
+
+    requestAnimationFrame(computeFrame);
+  }; // Begin animation
+
+
+  requestAnimationFrame(computeFrame);
+  window.addEventListener("scroll", scrollInProgress);
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,81 @@
-import React, { useState } from "react";
+"use strict";
+// @flow
+
+import React, { useState, useRef } from "react";
 
 export default function HomePage() {
+  /*
+    Includes code for Dynamic Home Page Title
+    Adapted from: https://www.sliderrevolution.com/resources/css-text-animation/
+    Animation: Cross Color Text Reveal
+    Developer: Kenny
+  */
+
   // TODO: Add dynamic quotes
+  const darkRef = useRef(null);
+  const lightRef = useRef(null);
+
+  setupDynamicTitle(darkRef, lightRef);
+
   return (
-    <div>
-      <video autoPlay muted loop id="desktop_home_background">
-        <source src="../media/pexels_pixabay_background.mp4" type="video/mp4" />
-      </video>
-      <img
-        id="mobile_home_background"
-        src="../media/mobile_home_background.png"
-        type="img/png"
-      />
-      <div className="content" className="full-parent-height">
-        <h1 className="display-1" id="church_name">
-          "Iglesia de Dios Ebenezer"
-        </h1>
-        <div id="home_intro">
-          <figure className="text-center">
-            <blockquote className="blockquote">
-              <h6 className="display-6">
-                "Tomó luego Samuel una piedra y la puso entre Mizpa y Sen, y le
-                puso por nombre Eben-ezer"
-              </h6>
-            </blockquote>
-            <figcaption className="blockquote-footer">
-              "1 Samuel 7:12"
-            </figcaption>
-          </figure>
+    <div id="home_page_container">
+      <div className="outer-container">
+        <div id="home-image" className="image-container">
+          <h2 className="section-title on-dark">
+            <span className="paralax-title" ref={darkRef}>
+              Iglesia de Dios Ebenezer
+            </span>
+          </h2>
         </div>
+        <h2 className="section-title">
+          <span className="paralax-title" ref={lightRef}>
+            Iglesia de Dios Ebenezer
+          </span>
+        </h2>
+      </div>
+      <div id="home_intro">
+        <figure className="text-center">
+          <blockquote className="blockquote">
+            <h6 className="display-6">
+              "Tomó luego Samuel una piedra y la puso entre Mizpa y Sen, y le
+              puso por nombre Eben-ezer"
+            </h6>
+          </blockquote>
+          <figcaption className="blockquote-footer">"1 Samuel 7:12"</figcaption>
+        </figure>
       </div>
     </div>
   );
+}
+
+/// Use black and white identical title elements to add cool animation
+///
+/// @param darkRef A useRef return value reqpresenting the black title
+/// @param lightRef A useRef return value reqpresenting the white title
+function setupDynamicTitle(darkRef, lightRef) {
+  let didScroll = false;
+
+  /*
+    When the user scrolls, trigger the conditional statement
+    inside the infinetly repeating computeFrame function
+  */
+  const scrollInProgress = () => {
+    didScroll = true;
+  };
+
+  const computeFrame = () => {
+    if (didScroll) {
+      darkRef.current.style.transform =
+        "translateX(" + window.scrollY / 10 + "%)";
+      lightRef.current.style.transform =
+        "translateX(" + window.scrollY / 10 + "%)";
+      didScroll = false;
+    }
+    // Recursively call computeFrame to cause constant frame computation
+    requestAnimationFrame(computeFrame);
+  };
+
+  // Begin animation
+  requestAnimationFrame(computeFrame);
+  window.addEventListener("scroll", scrollInProgress);
 }
